628a296953b50186520790f3be13c86de66d7be4
gourav5768 <kapoorgourav23@gmail.com>
2023-05-19 01:53:08 +0530

    fix header

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Contact from "../src/components/home/Contact";
 import About from "../src/components/home/About";
 import Roadmap from "../src/components/home/Roadmap";
 import Tokenomics from "../src/components/home/Tokenomics";
-import Header from "../src/components/home/header";
+import Header from "../src/components/home/Header";
 
 export default function Home() {
   return (
