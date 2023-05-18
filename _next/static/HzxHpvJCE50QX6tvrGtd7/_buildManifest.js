b3f0f050893928bf7ca68bb4dae852594a378e17
Kachowguy <>
2023-05-18 17:07:59 +0000

    Updates

---
+++ _next/static/HzxHpvJCE50QX6tvrGtd7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3e3445db503d6e9d.css","static/chunks/pages/index-9c85f16e428869d1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
