b39a41e733a46560bbb1b6679956335c27a687c0
gourav5768 <kapoorgourav23@gmail.com>
2023-05-20 00:36:22 +0530

    done

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -28,6 +28,40 @@ const Contact = () => {
         <br /> roadmap. the coin is completely useless and
         <br /> for entertainment purposes only.
       </p>
+      <a href='https://t.me/LightningMcCoin' target='_blank' rel='noreferrer'>
+        <p
+          style={{
+            cursor: "pointer",
+            wordBreak: "break-all",
+          }}>
+          https://t.me/LightningMcCoin
+        </p>
+      </a>
+      <a
+        href='https://twitter.com/kachowcoin?s=11'
+        target='_blank'
+        rel='noreferrer'>
+        <p
+          style={{
+            cursor: "pointer",
+            wordBreak: "break-all",
+          }}>
+          https://twitter.com/kachowcoin?s=11
+        </p>
+      </a>
+      <a
+        href='https://www.dextools.io/app/en/ether/pair-explorer/0x96c04cf05256d842e3b39325499f223717a979b8'
+        target='_blank'
+        rel='noreferrer'>
+        <p
+          style={{
+            paddingBottom: "6rem",
+            cursor: "pointer",
+            wordBreak: "break-all",
+          }}>
+          https://www.dextools.io/app/en/ether/pair-explorer/0x96c04cf05256d842e3b39325499f223717a979b8
+        </p>
+      </a>
     </div>
   );
 };
